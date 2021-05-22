0725cbf8fee864c9824c88370d8b6074e8254128
tdragoo <tdragoo@IPSSDC02DXKXB.local>
2021-05-22 07:44:37 -0500

    fixed some stuff

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Portfolio from './Components/Portfolio';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import awsmobile from './aws-exports.js';
+import * as mutations from "./graphql/mutations";
+import * as subscriptions from "./graphql/subscriptions";
+
+Amplify.configure(awsmobile);
 
 class App extends Component {
 
@@ -42,6 +48,56 @@ class App extends Component {
   }
 
   render() {
+    document.addEventListener("DOMContentLoaded", function () {
+        init();
+    });
+
+    const init = function createUpdateCounters() {
+        console.log("this has been called")
+        const countersToUpdate = document.querySelectorAll(`[data-counter-id]`);
+        const counterHitIdSet = new Set();
+
+        countersToUpdate.forEach((counter) => {
+              counterHitIdSet.add(counter.dataset.counterId);
+              console.log(counter.dataset.hits)
+        })
+
+        counterHitIdSet.forEach((id) => {
+              hitCounter(id);
+        });
+    }
+
+    /*
+    Send a mutation to your GraphQL to let it know we hit it.
+    This also means we get back the current count, including our own hit.
+    */
+    async function hitCounter(id) {
+    const counter = await API.graphql(graphqlOperation(mutations.hit, { input: { id } }));
+        updateText(counter.data.hit)
+        subscribeCounter(id)
+    }
+
+    function updateText(counter) {
+    const countersToUpdate = document.querySelectorAll(`[data-counter-id=${counter.id}]`);
+    countersToUpdate.forEach(function (elem) {
+        elem.innerHTML = counter.hits;
+        console.log(counter.hits)
+    })
+    }
+
+    /*
+    Subscribe via WebSockets to all future updates for the counters
+    we have on this page.
+    */
+    function subscribeCounter(id) {
+        const subscription = API.graphql(
+                graphqlOperation(subscriptions.hits, { id })
+          ).subscribe({
+                next: (counter) => updateText(counter.value.data.hits)
+          }
+        );
+    }
+
     return (
       <div className="App">
         <Header data={this.state.resumeData.main}/>
