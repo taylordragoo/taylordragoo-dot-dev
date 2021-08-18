dc2776d9aa0447bcf0e95e1226d787e2fe398f53
Taylor Dragoo <taylor-dragoo@ouhsc.edu>
2021-08-18 14:57:05 -0500

    adjusted skills text

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -60,7 +60,7 @@ class Resume extends Component {
       <div className="row skill">
 
          <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
+            <h1><span>Relative Skills</span></h1>
          </div>
 
          <div className="nine columns main-col">
